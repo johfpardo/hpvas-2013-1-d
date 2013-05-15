258f68b837900df78b98f2f313354ceb9053e7f5
John Fredy <John Fredy@JohnFredy-PC>
2013-05-14 23:16:50 -0500

    Commit by: John Fredy Pardo Changes:  Create Person and Veterinarian classes, so Create PersonDao and VeterinarianDao classes too - In the process also creates Appointment class

---
+++ hpvas-2013-1-d/src/dao/PersonDao.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entidad.Person;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author John Fredy
+ */
+public class PersonDao {
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("HPVAS");
+    
+    public void create(Person object) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+    
+    public boolean delete(Person object) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        boolean ret = false;
+        try {
+            em.remove(object);
+            em.getTransaction().commit();
+            ret = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            return ret;
+        }
+    }
+    
+    public Person read(Person object) {
+        EntityManager em = emf.createEntityManager();
+        Person person = null;
+        Query q = em.createQuery("SELECT p FROM Person p " + "WHERE p.nombre LIKE :nombre")
+                .setParameter("nombre", object.getName());
+        try {
+            person = (Person) q.getResultList().get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            return person;
+        }
+    }
+    
+    public boolean update(Person object,Person newObject) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        boolean ret = false;
+         try {
+            object = read(object);
+            object.setName(newObject.getName());
+            object.setLastName(newObject.getLastName());
+            object.setSSN(newObject.getSSN());
+            object.setBirthDate(newObject.getBirthDate());
+            object.setBirthPlace(newObject.getBirthPlace());
+            em.merge(object);
+            em.getTransaction().commit();
+            ret = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            return ret;
+        }
+    }
+}
---
+++ hpvas-2013-1-d/src/dao/VeterinarianDao.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entidad.Veterinarian;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author John Fredy
+ */
+public class VeterinarianDao {
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("HPVAS");
+    
+    public void create(Veterinarian object) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+    
+    public boolean delete(Veterinarian object) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        boolean ret = false;
+        try {
+            em.remove(object);
+            em.getTransaction().commit();
+            ret = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            return ret;
+        }
+    }
+    
+    public Veterinarian read(Veterinarian object) {
+        EntityManager em = emf.createEntityManager();
+        Veterinarian veterinarian = null;
+        Query q = em.createQuery("SELECT v FROM Veterinarian v " + "WHERE v.nombre LIKE :nombre")
+                .setParameter("nombre", object.getName());
+        try {
+            veterinarian = (Veterinarian) q.getResultList().get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            return veterinarian;
+        }
+    }
+    
+    public boolean update(Veterinarian object,Veterinarian newObject) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        boolean ret = false;
+         try {
+            object = read(object);
+            object.setName(newObject.getName());
+            object.setLastName(newObject.getLastName());
+            object.setSSN(newObject.getSSN());
+            object.setBirthDate(newObject.getBirthDate());
+            object.setBirthPlace(newObject.getBirthPlace());
+            object.setFocusOfPractice(newObject.getFocusOfPractice());
+            object.setSpecialities(newObject.getSpecialities());
+            object.setSalary(newObject.getSalary());
+            object.setGraduatedAt(newObject.getGraduatedAt());
+            em.merge(object);
+            em.getTransaction().commit();
+            ret = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+            return ret;
+        }
+    }
+    
+}
