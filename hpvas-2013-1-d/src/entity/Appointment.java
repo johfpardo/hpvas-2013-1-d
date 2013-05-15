8324a2e2c0ef9cffa221701c93b4f24eec4818af
sneiderhc <sneiderhc@SneiderHC-PC>
2013-05-15 11:21:16 -0500

    Commit by: Sneider Hernandez Changes: -Entity class Appointment and MedicalRecord added -Dao for class Appointment and MedicalRecord added -mysql-connector added

---
+++ hpvas-2013-1-d/src/entity/Appointment.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author John Fredy
+ */
+@Entity
+@DiscriminatorValue("Appointment")
+public class Appointment implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.TABLE)
+    @Column(name = "AppointmentID")
+    private Long id;
+        
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date date;
+    private int charge;
+    private String notes;
+    
+    @ManyToOne
+    private Veterinarian veterianarian;
+    @ManyToOne
+    private MedicalRecord medicalRecord;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getCharge() {
+        return charge;
+    }
+
+    public void setCharge(int charge) {
+        this.charge = charge;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
