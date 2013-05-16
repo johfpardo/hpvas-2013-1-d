76dbd2c01505532ee6cc1db47dae2e6cba136e78
Daniel <Daniel@Dan>
2013-05-16 04:05:46 -0500

    Commit by: Daniel Malaver basic Login classes and structure

---
+++ hpvas-2013-1-d/src/hpvas20131d/Login.java
@@ -0,0 +1,35 @@
+package hpvas20131d;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.DC72F6B2-37D8-E5C4-9657-3AC3AADFE643]
+// </editor-fold> 
+public class Login {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.DABE9A2A-DB78-D623-0121-3A36E9F6ABC1]
+    // </editor-fold> 
+    private ValidarLogin Login;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.9EF71CE5-2D4F-ACD1-2BD1-43C2265E3FDA]
+    // </editor-fold> 
+    public Login () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.8745B427-C34D-327B-A712-7466DA290464]
+    // </editor-fold> 
+    public ValidarLogin getLogin () {
+        return Login;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.E6E644AE-6D1C-7F26-BF6B-1518BC86B723]
+    // </editor-fold> 
+    public void setLogin (ValidarLogin val) {
+        this.Login = val;
+    }
+
+}
+
